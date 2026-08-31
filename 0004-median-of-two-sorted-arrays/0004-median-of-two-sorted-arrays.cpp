2ef7f17d72c599d56ec943ee2b8b46082fa6a06c
Anoop Singh <anoopsinghsuryavanshi027@gmail.com>
2026-08-31 19:45:13 +0530

    Time: 0 ms (100%), Space: 95.1 MB (61.27%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int n = n1 + n2;
+        // yeh function tab execute karega jab hamara pehla array bada hoga-->>
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) / 2;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1)
+                    return max(l1, l2);
+                return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
+
+            } else if (l1 > l2) {
+                high = mid1 - 1;
+
+            } else {
+                low = mid1 + 1;
+            }
+        }
+
+        return 0;
+    }
+};
+
+// note point-->>
+// isme sara khela mid ka hai
+
+// steps-->>
+// Median of Two Sorted Arrays — Steps
+// 1-Choose smaller array for binary search.
+// 2-Set low = 0, high = n1.
+
+// 3- Find partition:
+
+// mid1 = (low + high) / 2;
+// mid2 = left - mid1;
+// 4-Find l1, l2, r1, r2.
+
+// 5-Check valid partition:
+
+// l1 <= r2 && l2 <= r1
+// 6- If valid:
+// Odd: max(l1, l2)
+// Even: (max(l1,l2) + min(r1,r2)) / 2
+// 7- If l1 > r2 → move left.
+// 8- Otherwise → move right.
+
+// Complexity: O(log(min(n1,n2))) time, O(1) space.
